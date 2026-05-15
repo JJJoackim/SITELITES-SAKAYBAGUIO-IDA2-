f0fc914df74ecfc41c23b64f4b59137ffc12f1f1
euli-jpeg <20104339@s.ubaguio.edu>
2026-05-15 18:36:53 +0800

    Uploading of files part 2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    // Proxy API calls to your Node.js backend during development
     proxy: {
       "/api": {
         target: "http://localhost:5000",
@@ -13,4 +11,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
